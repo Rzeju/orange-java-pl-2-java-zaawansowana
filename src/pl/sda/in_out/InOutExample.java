26a788554aa3d03ef1682042a9f3f9d399838a5d
Piotr Andrzejak <pandrzejak93@gmail.com>
2023-01-21 15:10:50 +0100

    Map example

---
+++ src/pl/sda/in_out/InOutExample.java
@@ -0,0 +1,34 @@
+package pl.sda.in_out;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class InOutExample {
+
+    public static void main(String[] args) {
+
+        FileInputStream in;
+        FileOutputStream out;
+
+        try {
+            in = new FileInputStream("D:\\Dev\\SDA\\orange-java-pl-2-java-zaawansowana\\src\\pl\\sda\\resources\\user.txt");
+            out = new FileOutputStream("D:\\Dev\\SDA\\orange-java-pl-2-java-zaawansowana\\src\\pl\\sda\\resources\\user_output.txt");
+
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Brak pliku: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Problem z dostępem do pliku: " + e.getMessage());
+        }
+
+
+
+
+
+    }
+}
