f931afdcae37555bb3fb268b877a39bbc51682a1
Piotr Andrzejak <pandrzejak93@gmail.com>
2023-02-02 19:57:58 +0100

    concurent example

--- src/pl/sda/concurrency/atomics/AtomicExample.java
+++ src/pl/sda/concurrency/atomics/AtomicExample.java
@@ -1,24 +1,27 @@
-package pl.sda.concurrency.callable_future;
+package pl.sda.concurrency.atomics;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class Main {
+public class AtomicExample {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+
         List<Callable<Integer>> callableList = new ArrayList<>();
 
-        for(int i = 0; i < 100; i++) {
-            Callable<Integer> callable = DataBase::increment;
+        for (int i = 0; i < 100; i++) {
+            Callable<Integer> callable = () -> atomicInteger.addAndGet(1);
             callableList.add(callable);
         }
 
         ExecutorService executorService = Executors.newFixedThreadPool(5);
         List<Future<Integer>> myResponses = executorService.invokeAll(callableList);
 
-        System.out.println(DataBase.getCounter());
+        System.out.println(atomicInteger.get());
 
         for (Future<Integer> future : myResponses) {
             System.out.print(future.get() + " ");
--- src/pl/sda/concurrency/callable_future/DataBase.java
+++ src/pl/sda/concurrency/callable_future/DataBase.java
@@ -2,16 +2,20 @@ package pl.sda.concurrency.callable_future;
 
 public class DataBase {
 
-    private static int counter = 0;
+    private static volatile int counter = 0;
 
-    public static synchronized int increment() throws InterruptedException {
+    public static int increment() throws InterruptedException {
         int result = counter + 1;
-        Thread.sleep(100);
+        //Thread.sleep(100);
         counter = result;
         return counter;
     }
 
     public static int getCounter() {
         return counter;
+    }
+
+    public static String getCounter(int x) {
+        return "";
     }
 }
